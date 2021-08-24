fc87ccf640efa004640dd4409434efe2712c6e91
Muhammad Humza <humza.jameel@evantagesoft.com>
2021-08-24 17:01:02 +0500

    optimize code and added env file

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 
 //the extended: true precises that the req. body object will contain values of any type instead of just strings
@@ -14,12 +16,10 @@ app.use("/posts", postRoutes);
 
 //process.env.PORT = will be populated by the host
 const PORT = process.env.PORT || "5000";
-const CONNECTION_URL =
-  "mongodb+srv://humzajameel:Error500@cluster0.kndno.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 //useNew and useUni are for avoiding warnings and errors
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
